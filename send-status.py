86949aa94d0c8cc0158204c4f8230578c11f5351
Jason R. Coombs <jaraco@jaraco.com>
2017-02-15 20:28:09 -0500

    Add a variable for e-mails

--- send-status.py
+++ send-status.py
@@ -4,6 +4,12 @@ send-status.py
 Sends the latest OPM status to the e-mail address in the script.
 """
 
+
+TARGET_EMAILS = ['jaraco@jaraco.com']
+"""
+The e-mails to receive the notifications
+"""
+
 def main():
     pass
 
